6c22285dbbe88bebef4a29c57ff420b91e33df15
joe <joe.maliksi@gmail.com>
2023-02-08 23:39:12 -0500

    capy

--- capybr/src/App.js
+++ capybr/src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+    const [capy, setCapy] = useState("");
+    const [name, setName] = useState("");
+    const age = Math.floor(7 + Math.random() * 10 * 12);
+
+    useEffect(() => {
+        fetch('https://api.capy.lol/v1/capybara?json=true')
+        .then(response => {
+            if (!response.ok) {
+                return
+            }
+            return response.json();
+        })
+        .then(js => {
+            setCapy(js.data.url);
+        });
+
+        fetch('https://onomancer.sibr.dev/api/getName?threshold=4')
+        .then(response => {
+            if (!response.ok) {
+                return
+            }
+            return response.json();
+        })
+        .then(js => {
+            setName(js);
+        });
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={capy}/>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
     </div>
   );
 }
