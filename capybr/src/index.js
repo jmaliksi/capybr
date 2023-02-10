acd884c59d4c64ac177586abee5c9e44825ef94d
joe <joe.maliksi@gmail.com>
2023-02-10 00:08:39 -0500

    social tags

--- capybr/src/index.js
+++ capybr/src/index.js
@@ -11,17 +11,17 @@ root.render(
         <Helmet>
             <title>capybr</title>
             <meta charSet="UTF-8"/>
-            <meta name="description" content="find the capybara of your dreams"/>
+            <meta name="description" content="swipe the capybara of your dreams!"/>
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://jmaliksi.github.io/capybr"/>
             <meta property="og:title" content="capybr"/>
-            <meta property="og:description" content="find the capybara of your dreams"/>
+            <meta property="og:description" content="swipe the capybara of your dreams!"/>
 
             
             <meta property="twitter:url" content="https://jmaliksi.github.io/capybr"/>
             <meta property="twitter:title" content="capybr"/>
-            <meta property="twitter:description" content="find the capybara of your dreams"/>
+            <meta property="twitter:description" content="swipe the capybara of your dreams!"/>
         </Helmet>
         <App />
     </>
