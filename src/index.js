fabf593b6a1687c1d7543c8b64e7caa10c1f8e3f
joe <joe.maliksi@gmail.com>
2023-03-05 10:09:13 -0500

    fix fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "typeface-inter";
+import "@fontsource/inter";
 import './index.css';
 import App from './App';
 import Modal from 'react-modal';
